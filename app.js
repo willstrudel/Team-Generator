0df96769721cd66663f2a62d230bbe0becf711c8
Will <willstruder@gmail.com>
2021-07-13 20:13:03 -0400

    Created HTML pages for team and Manager

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+const Manager = require("./js/Manager");
+const Engineer = require("./js/Engineer");
+const Intern = require("./js/Intern");
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const employeeList = [];
 
 const promptUser = () => 
     inquirer.prompt([
